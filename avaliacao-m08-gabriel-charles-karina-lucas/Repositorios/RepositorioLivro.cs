4d771b2bb1c8d4cf9d8c8e20a4bea1ecb718364f
gabriel-becker <gabriel.alexandre.becker@gmail.com>
2026-06-18 19:55:18 -0300

    implementado-repositorio-de-livros-e-metodos-para-manipular

--- avaliacao-m08-gabriel-charles-karina-lucas/Repositorios/RepositorioLivro.cs
+++ avaliacao-m08-gabriel-charles-karina-lucas/Repositorios/RepositorioLivro.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using avaliacao_m08_gabriel_charles_karina_lucas.Modelos;
 
 namespace avaliacao_m08_gabriel_charles_karina_lucas.Repositorios
 {
-    internal class RepositorioLivro
+    public class RepositorioLivro
     {
+
+        public List<Livro> Livros = new List<Livro>
+        {
+
+
+        };
+
+        public void Adicionar(Livro livro)
+        {
+            Livros.Add(livro);
+        }
+
+        public Livro BuscarPorId(int id)
+        {
+            Livro livroEncontrado = Livros.FirstOrDefault(l => l.Id == id);
+            return livroEncontrado;
+        }
+
+        public List<Livro> ListarTodos()
+        {
+            List<Livro> livrosOrdenados = Livros.OrderByDescending(l => l.Titulo);
+            return livrosOrdenados;
+        }
+
+        public Livro BuscarPorAutor(string autor);
+        {
+            var livroEncontrado = Livros.FirstOrDefault(l => l.Autor == autor);
+            return livroEncontrado;
+        }
     }
 }
