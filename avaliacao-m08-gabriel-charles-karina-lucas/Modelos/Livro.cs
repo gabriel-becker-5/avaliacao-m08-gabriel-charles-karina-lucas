609840a7d617f83f7ad210c6f52c6d203785860e
Charles Junior <charles64339146@alunos.sc.senac.br>
2026-06-18 20:24:10 -0300

    implementado_classeLivro

--- avaliacao-m08-gabriel-charles-karina-lucas/Modelos/Livro.cs
+++ avaliacao-m08-gabriel-charles-karina-lucas/Modelos/Livro.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace avaliacao_m08_gabriel_charles_karina_lucas.Modelos
 {
-    internal class Livro
+     public class Livro
     {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Ano { get; set; }
+        public bool Disponivel { get; set; }
+
+        public Livro(int id, string titulo, string autor, int ano, bool disponivel)
+        {
+            Id = id;
+            Titulo = titulo;
+            Autor = autor;
+            Ano = ano;
+            Disponivel = disponivel;
+        }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Título: {Titulo}");
+            Console.WriteLine($"Autor: {Autor}");
+            Console.WriteLine($"Ano: {Ano}");
+            Console.WriteLine($"Disponível: {(Disponivel ? "Sim" : "Não")}");
+        }
+
     }
 }
